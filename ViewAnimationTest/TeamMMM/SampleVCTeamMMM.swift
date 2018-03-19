f419fddbbcb2fb69ee32febfb2a8c7cce75a2909
SunTelematics <suntelematics@sunttelematicsmac.local>
2018-03-19 18:35:39 +0530

    Print st added

--- ViewAnimationTest/TeamMMM/SampleVCTeamMMM.swift
+++ ViewAnimationTest/TeamMMM/SampleVCTeamMMM.swift
@@ -13,6 +13,7 @@ class SampleVCTeamMMM: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        print("MMM")
         // Do any additional setup after loading the view.
     }
 
